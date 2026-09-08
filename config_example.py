ea0abdec5d1bd0e1ac9b0dd68782f8e8b47586db
ealaindesign <design@ealain.com.au>
2026-09-08 23:30:01 +1000

    Added example of config. Don't use this, it's garbage data

---
+++ config_example.py
@@ -0,0 +1,24 @@
+# Auto-generated configuration by setup_wizard.pyw
+
+# Wi-Fi Regulatory & Credentials
+RP2_COUNTRY = "CH"
+WIFI_SSID = "Pi_2.4ghz"
+WIFI_PASS = "potatofarmer"
+
+# Inverter Connection (Modbus TCP)
+INVERTER_IP = "192.168.1.35"
+INVERTER_PORT = 502
+UNIT_ID = 247
+
+# Feed-In Tariffs
+CURRENCY_SYMBOL = "$"
+TARIFF_RATE = 0.17
+
+# Promotional / Locked Baseline Tariffs
+BASELINE_KWH = 0.0
+BASELINE_VALUE = 0.0
+
+# Location & UTC Offset (for Sunrise/Sunset & Time Sync)
+UTC_OFFSET_HOURS = 10.0
+FALLBACK_LAT = -10.5526
+FALLBACK_LON = 186.00025235
